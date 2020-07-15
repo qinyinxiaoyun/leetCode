94a7e898098d9a591ecefbd0a3517d7b4e9363d7
qinyinxiaoyun <18245036119@163.com>
2020-07-15 08:49:59 +0800

    traceback

---
+++ src/com/backtrace/P46Solution.java
@@ -0,0 +1,17 @@
+package com.backtrace;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P46Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+
+
+    }
+
+    private void permutation(int[] nums,List<List<Integer>> result,int i){
+        if (nums.length == i)return;
+        
+    }
+}
